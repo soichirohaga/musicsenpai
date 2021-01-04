490e20c94fe054646bbe88d82b6008b5c4d76907
芳賀壮一郎 <soichi@mba.local>
2021-01-04 20:34:41 +0900

    add code

--- app/Http/Controllers/SearchVideoController.php
+++ app/Http/Controllers/SearchVideoController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class SearchVideoController extends Controller
 {
-    //
+    public function index()
+    {
+        
+
+        return view('users/videos');
+    }
 }
